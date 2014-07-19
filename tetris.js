c77e262bafba57b1bb33bef0e20ee41e316b7f9e
Yolka <yolenok@gmail.com>
2014-07-19 13:10:14 +0300

    My initial commit

---
+++ tetris.js
@@ -0,0 +1,14 @@
+ /*var example = document.getElementById("example");
+ $(document).ready(function() {if
+ 	document.createElement("canvas"))}
+  ctx     = example.getContext('2d');
+  ctx.fillRect(0, 0, example.width, example.height);*/
+
+  jQuery(document).ready(function() {
+
+  	var example = document.getElementById("example");
+	ctx     = example.getContext('2d');
+  	ctx.fillRect(0, 0, example.width, example.height);
+
+  });
+
