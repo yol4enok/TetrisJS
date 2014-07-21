43579773838ff32ae9963109e76520297e07aa53
Yolka <yolenok@gmail.com>
2014-07-21 19:35:17 +0300

    added figura

--- tetris.js
+++ tetris.js
@@ -2,36 +2,36 @@
   /*var canvas = document.getElementById("example");
 	ctx     = canvas.getContext('2d');
 	
-  	ctx.fillRect(0, 0, example.width, example.height);
-    
-    jQuery(ctx).animate({color : "#FFFFFF"}, 1000);
-  	//ctx.fillRect(1, 1, example.width, example.height);
-    /*function step(x,y) {
+	ctx.fillRect(0, 0, example.width, example.height);
+	
+	jQuery(ctx).animate({color : "#FFFFFF"}, 1000);
+	//ctx.fillRect(1, 1, example.width, example.height);
+	/*function step(x,y) {
   requestAnimationFrame(step);
   for (x=0 , x =< example.width,++x){
-    for (y=0, y=< example.height,++y){
-      ctx.fillstyle()
-    }
+	for (y=0, y=< example.height,++y){
+	  ctx.fillstyle()
+	}
   }
 }
 step();*/
 
   //})
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
+	window.requestAnimFrame = (function(){
+	  return  window.requestAnimationFrame       || 
+			  window.webkitRequestAnimationFrame || 
+			  window.mozRequestAnimationFrame    || 
+			  window.oRequestAnimationFrame      || 
+			  window.msRequestAnimationFrame     || 
+			  function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			  };
+	})();
   
 
 
-var canvas, context, currentX, currentY, grid;
+var canvas, context, currentX, currentY, grid, matrix;
 
 var prevTime;
 var curTime;
@@ -42,7 +42,7 @@ var color = {
 };
 
 function rgbToHex(r, g, b) {
-    return "#" + ((1 << 24) + (parseInt(r) << 16) + (parseInt(g) << 8) + parseInt(b)).toString(16).slice(1);
+	return "#" + ((1 << 24) + (parseInt(r) << 16) + (parseInt(g) << 8) + parseInt(b)).toString(16).slice(1);
 }
 
 init();
@@ -50,103 +50,124 @@ animate();
 
 function init() {
 
-    canvas = document.getElementById( 'example' );
-    canvas.width = 256;
-    canvas.height = 256;
+	canvas = document.getElementById( 'example' );
+	canvas.width = 256;
+	canvas.height = 256;
 
-    context = canvas.getContext( '2d' );
-    currentX = 0;
-    currentY = 0;
+	context = canvas.getContext( '2d' );
+	currentX = 0;
+	currentY = 0;
 
-    context.fillStyle = '#000000';
-    context.fillRect( 0, 0, 255, 255 );
+	context.fillStyle = '#000000';
+	context.fillRect( 0, 0, 255, 255 );
 	  prevTime = curTime = 0;
 
-    grid = new gameGrid(canvas.width, canvas.height, 16, 16);
-   // matrix[1][1] = 1;
-   console.log(grid);
+	grid = new gameGrid(canvas.width, canvas.height, 16, 16);
+	matrix[1][1] = 1;
+	console.log(grid);
 }
 
 function animate() {
-    requestAnimFrame( animate );
-    draw();
+	requestAnimFrame( animate );
+	draw();
 }
 
 function draw() {
-  console.log(grid.canvH);
-    curTime = new Date().getTime();
-	var dt = curTime - prevTime;
-    if(dt >= 16) {
-		if (currentX < canvas.width - 1) {
-		  currentX++;
-		}
+  /*console.log(grid.canvH);
+	curTime = new Date().getTime();
+  var dt = curTime - prevTime;
+	if(dt >= 16) {
+	if (currentX < canvas.width - 1) {
+	  currentX++;
+	}
 
-		if (currentY < canvas.height - 1) {
-		  currentY++;
-		}
-		color.r += 0.01 * dt;
-		color.g += 0.01 * dt;
-		color.b += 0.01 * dt;
-		if(color.r > 255) {
-			color.r = color.g = color.b = 0;
-		}
-		context.fillStyle = rgbToHex(color.r, color.g, color.b);
-		context.fillRect( 0, 0, canvas.width, canvas.height );
-		
-/* 		if (currentX === canvas.width - 1 && currentY === canvas.height -1 ) {
+	if (currentY < canvas.height - 1) {
+	  currentY++;
+	}
+	color.r += 0.01 * dt;
+	color.g += 0.01 * dt;
+	color.b += 0.01 * dt;
+	if(color.r > 255) {
+	  color.r = color.g = color.b = 0;
+	}
+	context.fillStyle = rgbToHex(color.r, color.g, color.b);
+	context.fillRect( 0, 0, canvas.width, canvas.height );*/
+	
+	drawobject(grid.gridWidth,grid.gridHeight,grid.cellW, grid.cellH);
+	
+/*    if (currentX === canvas.width - 1 && currentY === canvas.height -1 ) {
 		  currentX = 0;
 		  currentY = 0;
 		  
 		  context.fillStyle = '#000000';
 		  context.fillRect( 0, 0, 255, 255 );
 		} */
-		prevTime = curTime;
-    }
+		/*prevTime = curTime;
+	}*/
 
-    /*var time = new Date().getTime() * 0.002;
-    var x = Math.sin( time ) *128;
-    var y = Math.cos( time * 0.9 ) * 96 + 128;*/
-    /*var time = new Date().getTime() * 0.002;
-    var x =0;
-    var y =0;
-    for (var x, x<canvas.width,++x){
-      for (var y, y<canvas.heigth,++y){
-          context.fillRect(x,y,x+1,)
-      }
+	/*var time = new Date().getTime() * 0.002;
+	var x = Math.sin( time ) *128;
+	var y = Math.cos( time * 0.9 ) * 96 + 128;*/
+	/*var time = new Date().getTime() * 0.002;
+	var x =0;
+	var y =0;
+	for (var x, x<canvas.width,++x){
+	  for (var y, y<canvas.heigth,++y){
+		  context.fillRect(x,y,x+1,)
+	  }
 
-    }*/
-    /*context.fillStyle = '#000000';
-    context.fillRect( 0, 0, 255, 255 );
+	}*/
+	/*context.fillStyle = '#000000';
+	context.fillRect( 0, 0, 255, 255 );
 
 
-    context.fillStyle = '#FFFFFF';
-    context.beginPath();
-    context.arc( x, y, 10, 0, Math.PI * 2, true );
+	context.fillStyle = '#FFFFFF';
+	context.beginPath();
+	context.arc( x, y, 10, 0, Math.PI * 2, true );
 
-    ty
-    context.closePath();
-    context.fill();*/
+	ty
+	context.closePath();
+	context.fill();*/
 
 }
 
-   function gameGrid (canvW,canvH,cellwidth,cellheight){
-    this.canvW = canvW;
-    this.canvH = canvH;
-     this.cellW = cellwidth;
-    this.cellH = cellheight;
-    this.gridWidth = canvW/cellwidth;
-    this.gridHeight = canvH/cellheight;
-    matrix=[];
-    for  (var i=0; i<this.gridWidth; ++i){
-      matrix[i]=[];
-      for (var j=0; j<this.gridHeight; ++j){
-        matrix[i][j]=0;
+	function gameGrid (canvW,canvH,cellwidth,cellheight){
+		this.canvW = canvW;
+		this.canvH = canvH;
+		this.cellW = cellwidth;
+		this.cellH = cellheight;
+		this.gridWidth = canvW/cellwidth;
+		this.gridHeight = canvH/cellheight;
+		matrix=[];
+		for  (var i=0; i<this.gridWidth; ++i){
+			matrix[i]=[];
+			for (var j=0; j<this.gridHeight; ++j){
+				matrix[i][j]=0;
+		  }
+		}
+		this.matrix = matrix;
+	}
 
-      }
-    }
-    this.matrix = matrix;
+  function drawobject (gridWidth,gridHeight,cellW, cellH){
+	for (var i=0; i<gridWidth;++i){
+	  for (var j=0; j<gridHeight;++j){
+		if (matrix[i][j] == 1) {
+		  var x = i*cellW;
+		  var y = j*cellH;
+		  context.fillStyle ='#ff3df0';
+		  context.fillRect(x,y,cellW, cellH);
+
+		}
+
+	  }
+	}
+
   }
-  
+
+
+
+
+
   })
 
 
